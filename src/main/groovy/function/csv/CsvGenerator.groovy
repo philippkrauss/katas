ae6249296ceb9fe048c55d313bbf276c8d05651b
Philipp Krauss <philipp.krauss@codecentric.de>
2019-12-06 15:54:51 +0100

    Implement CsvGenerator kata

--- src/main/groovy/function/csv/CsvGenerator.groovy
+++ src/main/groovy/function/csv/CsvGenerator.groovy
@@ -1,4 +1,70 @@
 package function.csv
 
+/**
+ * https://ccd-school.de/coding-dojo/function-katas/csv-tabellieren/
+ */
 class CsvGenerator {
+
+	/**
+	 * input:
+	 * Name;Strasse;Ort;Alter
+	 * Peter Pan;Am Hang 5;12345 Einsam;42
+	 * Maria Schmitz;Kölner Straße 45;50123 Köln;43
+	 * Paul Meier;Münchener Weg 1;87654 München;65
+	 *
+	 * output:
+	 * Name         |Strasse         |Ort          |Alter|
+	 * -------------+----------------+-------------+-----+
+	 * Peter Pan    |Am Hang 5       |12345 Einsam |42   |
+	 * Maria Schmitz|Kölner Straße 45|50123 Köln   |43   |
+	 * Paul Meier   |Münchener Weg 1 |87654 München|65   |
+	 *
+	 * requirements:
+	 * - values separated by semicolon
+	 * - no special cases like semicolons inside values need to be handled
+	 * - input is always correct, no error handling necessary
+	 * - output is a formatted table with input data
+	 * - first line of input is interpreted as heading
+	 * - heading is separated from data by separation line
+	 * - column width is defined by the widest data point (heading or value)
+	 */
+	String generateCsv(String input) {
+		String[] lines = input.readLines()
+		String[][] matrix = lines.collect { it.split(';') }
+		int[] columnSizes = calculateColumnSizes(matrix)
+		String[] headers = matrix[0]
+		String result = generateLine(headers, columnSizes)
+		result += generateSeparatorLine(headers, columnSizes)
+		matrix.tail().each {
+			result += generateLine(it, columnSizes)
+		}
+		result
+	}
+
+	private String generateLine(def headerList, def columnSizes) {
+		String result = (headerList as List).withIndex().collect { element, index ->
+			element.padRight(columnSizes[index])
+		} join('|')
+		result + '|\n'
+	}
+
+	private String generateSeparatorLine(def headerList, def columnSizes) {
+		def individualSeparationLines = (headerList as List).withIndex().collect { element, index ->
+			'-'.multiply(columnSizes[index] as int)
+		}
+		String separationLine = individualSeparationLines.join('+')
+		separationLine + '+\n'
+	}
+
+	int[] calculateColumnSizes(String[][] matrix) {
+		int[] columnSizes = new int[matrix[0].length]
+		matrix.each {
+			it.collect { it.size() } eachWithIndex { int entry, int index ->
+				if (columnSizes[index] < entry) {
+					columnSizes[index] = entry
+				}
+			}
+		}
+		columnSizes
+	}
 }
--- src/test/groovy/function/csv/CsvGeneratorSpec.groovy
+++ src/test/groovy/function/csv/CsvGeneratorSpec.groovy
@@ -1,6 +1,71 @@
 package function.csv
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class CsvGeneratorSpec extends Specification {
+
+	@Unroll
+	void 'generate heading (#description)'() {
+		when:
+		String result = new CsvGenerator().generateCsv(input)
+		then:
+		result.startsWith(output)
+		where:
+		input                    | output                      | description
+		'Name'                   | 'Name|\n'                   | 'single item'
+		'Name;Strasse;Ort;Alter' | 'Name|Strasse|Ort|Alter|\n' | 'multiple items'
+		'Name\nName'             | 'Name|'                     | 'multiple lines'
+		'Key\nValue'             | 'Key  |'                    | 'multiple lines, values have different sizes'
+		'Key;LongKey\nValue;val' | 'Key  |LongKey|'            | 'multiple lines and values with different sizes'
+	}
+
+	@Unroll
+	void 'generate separation line (#description)'() {
+		when:
+		String result = new CsvGenerator().generateCsv(input)
+		then:
+		result.split('\n')[1] == separationLine
+		where:
+		input                    | separationLine            | description
+		'Name'                   | '----+'                   | 'single item'
+		'Name;Strasse;Ort;Alter' | '----+-------+---+-----+' | 'multiple items'
+		'Name\nName'             | '----+'                   | 'multiple lines'
+		'Key\nValue'             | '-----+'                  | 'multiple lines, values have different sizes'
+		'Key;LongKey\nValue;val' | '-----+-------+'          | 'multiple lines and values with different sizes'
+	}
+
+	@Unroll
+	void 'generate columns (#description)'() {
+		when:
+		String result = new CsvGenerator().generateCsv(input)
+		then:
+		result.split('\n')[2] == columnLine
+		where:
+		input                    | columnLine       | description
+		'hdr1;hdr2\nval1;val2'   | 'val1|val2|'     | 'multiple values, all same size'
+		'Name\nName'             | 'Name|'          | 'multiple lines'
+		'Key\nValue'             | 'Value|'         | 'multiple lines, value larger than heading'
+		'LongKey\nValue'         | 'Value  |'       | 'multiple lines, value smaller than heading'
+		'Key;LongKey\nValue;val' | 'Value|val    |' | 'multiple lines and values with different sizes'
+	}
+
+	void 'test with test data from requirements'() {
+		def testData = new StringBuilder()
+		testData << 'Name;Strasse;Ort;Alter' << '\n'
+		testData << 'Peter Pan;Am Hang 5;12345 Einsam;42' << '\n'
+		testData << 'Maria Schmitz;Kölner Straße 45;50123 Köln;43' << '\n'
+		testData << 'Paul Meier;Münchener Weg 1;87654 München;65'
+
+		def expectedResult = new StringBuilder()
+		expectedResult << 'Name         |Strasse         |Ort          |Alter|' << '\n'
+		expectedResult << '-------------+----------------+-------------+-----+' << '\n'
+		expectedResult << 'Peter Pan    |Am Hang 5       |12345 Einsam |42   |' << '\n'
+		expectedResult << 'Maria Schmitz|Kölner Straße 45|50123 Köln   |43   |' << '\n'
+		expectedResult << 'Paul Meier   |Münchener Weg 1 |87654 München|65   |' << '\n'
+		when:
+		String result = new CsvGenerator().generateCsv(testData as String)
+		then:
+		result == expectedResult.toString()
+	}
 }
